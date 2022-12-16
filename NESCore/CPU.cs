d2afc4e59285395ed9b84b02ee993844ab50e3d9
Attila Szücs <ati.szucs@hotmail.com>
2022-12-17 00:59:25 +0100

    LDX Immediate (0xA2) instruction Status register

--- NESCore/CPU.cs
+++ NESCore/CPU.cs
@@ -6,8 +6,36 @@
         byte A { get; set; }
         byte X { get; set; }
         byte Y { get; set; }
+
+        /*
+         * Bit 0 Carry
+         * Bit 1 Zero
+         * Bit 2 Interrupt disable
+         * Bit 3 Decimal
+         * Bit 4 --
+         * Bit 5 --
+         * Bit 6 Overflow
+         * Bit 7 Negative
+         */
         byte STATUS { get; set; }
         byte SP { get; set; }
+
+        void SetCarryFlag(bool flag);
+        void SetZeroFlag(bool flag);
+        void SetInterruptDisableFlag(bool flag);
+        void SetDecimalFlag(bool flag);
+        void SetOverflowFlag(bool flag);
+        void SetNegativeFlag(bool flag);
+    }
+
+    public class FlagPositions
+    {
+        public const byte CARRY = 0b00000001;
+        public const byte ZERO = 0b00000010;
+        public const byte INTERRUPT_DISABLE = 0b00000100;
+        public const byte DECIMAL = 0b00001000;
+        public const byte OVERFLOW = 0b01000000;
+        public const byte NEGATIVE = 0b10000000;
     }
 
     public class Registers : IRegisters
@@ -22,7 +50,89 @@
 
         public byte STATUS { get; set; } = 0;
 
-        public byte SP { get; set; } = 0; 
+        public byte SP { get; set; } = 0;
+
+        public void SetCarryFlag(bool flag)
+        {
+            if(flag)
+            {
+                EnableFlag(FlagPositions.CARRY);
+            }
+            else
+            {
+                DisableFlag(FlagPositions.CARRY);
+            }
+        }
+
+        public void SetDecimalFlag(bool flag)
+        {
+            if (flag)
+            {
+                EnableFlag(FlagPositions.DECIMAL);
+            }
+            else
+            {
+                DisableFlag(FlagPositions.DECIMAL);
+            }
+        }
+
+        public void SetInterruptDisableFlag(bool flag)
+        {
+            if (flag)
+            {
+                EnableFlag(FlagPositions.INTERRUPT_DISABLE);
+            }
+            else
+            {
+                DisableFlag(FlagPositions.INTERRUPT_DISABLE);
+            }
+        }
+
+        public void SetNegativeFlag(bool flag)
+        {
+            if (flag)
+            {
+                EnableFlag(FlagPositions.NEGATIVE);
+            }
+            else
+            {
+                DisableFlag(FlagPositions.NEGATIVE);
+            }
+        }
+
+        public void SetOverflowFlag(bool flag)
+        {
+            if (flag)
+            {
+                EnableFlag(FlagPositions.OVERFLOW);
+            }
+            else
+            {
+                DisableFlag(FlagPositions.OVERFLOW);
+            }
+        }
+
+        public void SetZeroFlag(bool flag)
+        {
+            if (flag)
+            {
+                EnableFlag(FlagPositions.ZERO);
+            }
+            else
+            {
+                DisableFlag(FlagPositions.ZERO);
+            }
+        }
+
+        private void EnableFlag(byte mask)
+        {
+            STATUS |= mask;
+        }
+
+        private void DisableFlag(byte mask)
+        {
+            STATUS &= (byte)~mask;
+        }
     }
 
     public class CPU : ICPU
--- NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
+++ NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
@@ -22,5 +22,51 @@ namespace NESCoreTests.Unit.CPUTest
             registers.VerifySet(r => r.PC = 0xdead);
             cycles.Should().Be(3);
         }
+
+        [Fact]
+        public void LDX_IMM()
+        {
+            var registers = new Mock<IRegisters>();
+            var bus = new Mock<IBUS>();
+            bus.Setup(b => b.Read(It.IsAny<UInt16>())).Returns(0x10);
+
+            var ldx_imm = new CPUInstructions().InstructionSet[Opcodes.LDX_IMM];
+            var cycles = ldx_imm(bus.Object, registers.Object);
+
+            registers.VerifySet(r => r.X = 0x10);
+            registers.Verify(r => r.SetNegativeFlag(false));
+            registers.Verify(r => r.SetZeroFlag(false));
+            cycles.Should().Be(2);
+        }
+
+        [Fact]
+        public void LDX_IMM_sets_zero_flag()
+        {
+            var registers = new Mock<IRegisters>();
+            var bus = new Mock<IBUS>();
+            bus.Setup(b => b.Read(It.IsAny<UInt16>())).Returns(0);
+
+            var ldx_imm = new CPUInstructions().InstructionSet[Opcodes.LDX_IMM];
+            var cycles = ldx_imm(bus.Object, registers.Object);
+
+            registers.VerifySet(r => r.X = 0);
+            registers.Verify(r => r.SetZeroFlag(true));
+            cycles.Should().Be(2);
+        }
+
+        [Fact]
+        public void LDX_IMM_sets_negative_flag()
+        {
+            var registers = new Mock<IRegisters>();
+            var bus = new Mock<IBUS>();
+            bus.Setup(b => b.Read(It.IsAny<UInt16>())).Returns(unchecked((byte)-1));
+
+            var ldx_imm = new CPUInstructions().InstructionSet[Opcodes.LDX_IMM];
+            var cycles = ldx_imm(bus.Object, registers.Object);
+
+            registers.VerifySet(r => r.X = unchecked((byte)-1));
+            registers.Verify(r => r.SetNegativeFlag(true));
+            cycles.Should().Be(2);
+        }
     }
 }
