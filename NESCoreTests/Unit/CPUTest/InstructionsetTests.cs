3c277b402b0e7ca1f4006228486334846a0af78a
Attila Szücs <ati.szucs@hotmail.com>
2022-12-17 16:13:56 +0100

    NOP does not affect the STATUS register test

--- NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
+++ NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
@@ -128,11 +128,23 @@ namespace NESCoreTests.Unit.CPUTest
         public void NOP()
         {
             var bus = new Mock<IBUS>();
-            var registers = new Registers();
+            var registers = new Mock<IRegisters>();
             var nop = new CPUInstructions().InstructionSet[Opcodes.NOP];
-            var cycles = nop(bus.Object, registers);
+            var cycles = nop(bus.Object, registers.Object);
 
-            registers.STATUS.Should().Be(0);
+            registers.Verify(r => r.SetCarryFlag(true), Times.Never());
+            registers.Verify(r => r.SetCarryFlag(false), Times.Never());
+            registers.Verify(r => r.SetZeroFlag(true), Times.Never());
+            registers.Verify(r => r.SetZeroFlag(false), Times.Never());
+            registers.Verify(r => r.SetDecimalFlag(true), Times.Never());
+            registers.Verify(r => r.SetDecimalFlag(false), Times.Never());
+            registers.Verify(r => r.SetInterruptDisableFlag(true), Times.Never());
+            registers.Verify(r => r.SetInterruptDisableFlag(false), Times.Never());
+            registers.Verify(r => r.SetNegativeFlag(true), Times.Never());
+            registers.Verify(r => r.SetNegativeFlag(false), Times.Never());
+            registers.Verify(r => r.SetOverflowFlag(true), Times.Never());
+            registers.Verify(r => r.SetOverflowFlag(false), Times.Never());
+
             cycles.Should().Be(2);
         }
     }
