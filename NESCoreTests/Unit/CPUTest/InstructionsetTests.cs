1dda7b7af3bc781e52fc6dada7c396fdb838300f
Attila Szücs <ati.szucs@hotmail.com>
2022-12-21 18:13:02 +0100

    Implemented STA_ZERO (0x85)

--- NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
+++ NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
@@ -116,6 +116,34 @@ namespace NESCoreTests.Unit.CPUTest
         }
 
         [Fact]
+        public void STA_ZERO()
+        {
+            var registers = new Mock<IRegisters>();
+            registers.Setup(r => r.A).Returns(10);
+            var bus = new Mock<IBUS>();
+            bus.Setup(b => b.Read(It.IsAny<UInt16>())).Returns(0xde);
+            var sut = new CPUInstructions().InstructionSet[Opcodes.STA_ZERO];
+            var cycles = sut(bus.Object, registers.Object);
+            bus.Verify(b => b.Write(0x00de, 10));
+
+            //No flags affected
+            registers.Verify(r => r.SetCarryFlag(true), Times.Never());
+            registers.Verify(r => r.SetCarryFlag(false), Times.Never());
+            registers.Verify(r => r.SetZeroFlag(true), Times.Never());
+            registers.Verify(r => r.SetZeroFlag(false), Times.Never());
+            registers.Verify(r => r.SetDecimalFlag(true), Times.Never());
+            registers.Verify(r => r.SetDecimalFlag(false), Times.Never());
+            registers.Verify(r => r.SetInterruptDisableFlag(true), Times.Never());
+            registers.Verify(r => r.SetInterruptDisableFlag(false), Times.Never());
+            registers.Verify(r => r.SetNegativeFlag(true), Times.Never());
+            registers.Verify(r => r.SetNegativeFlag(false), Times.Never());
+            registers.Verify(r => r.SetOverflowFlag(true), Times.Never());
+            registers.Verify(r => r.SetOverflowFlag(false), Times.Never());
+
+            cycles.Should().Be(3);
+        }
+
+        [Fact]
         public void STX_ZERO()
         {
             var registers = new Mock<IRegisters>();
@@ -129,6 +157,7 @@ namespace NESCoreTests.Unit.CPUTest
 
             bus.Verify(b => b.Write(0xaa, 10));
 
+            //No flags affected
             registers.Verify(r => r.SetCarryFlag(true), Times.Never());
             registers.Verify(r => r.SetCarryFlag(false), Times.Never());
             registers.Verify(r => r.SetZeroFlag(true), Times.Never());
