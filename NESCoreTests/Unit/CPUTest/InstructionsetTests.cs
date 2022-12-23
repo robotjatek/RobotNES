e00b91fbaa41b9fdde83e0ca336a7fede1b44fec
Attila Szücs <ati.szucs@hotmail.com>
2022-12-23 11:57:50 +0100

    SEI intruction (0x78)

--- NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
+++ NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
@@ -266,6 +266,30 @@ namespace NESCoreTests.Unit.CPUTest
         }
 
         [Fact]
+        public void SEI()
+        {
+            var bus = new Mock<IBUS>();
+            var registers = new Mock<IRegisters>();
+            registers.SetupAllProperties();
+            var sei = new CPUInstructions().InstructionSet[Opcodes.SEI];
+            var cycles = sei(bus.Object, registers.Object);
+
+            registers.Verify(r => r.SetCarryFlag(true), Times.Never);
+            registers.Verify(r => r.SetZeroFlag(true), Times.Never());
+            registers.Verify(r => r.SetZeroFlag(false), Times.Never());
+            registers.Verify(r => r.SetDecimalFlag(true), Times.Never());
+            registers.Verify(r => r.SetDecimalFlag(false), Times.Never());
+            registers.Verify(r => r.SetInterruptDisableFlag(true), Times.Once());
+            registers.Verify(r => r.SetInterruptDisableFlag(false), Times.Never());
+            registers.Verify(r => r.SetNegativeFlag(true), Times.Never());
+            registers.Verify(r => r.SetNegativeFlag(false), Times.Never());
+            registers.Verify(r => r.SetOverflowFlag(true), Times.Never());
+            registers.Verify(r => r.SetOverflowFlag(false), Times.Never());
+
+            cycles.Should().Be(2);
+        }
+
+        [Fact]
         public void BCS_doesNotTakeTheBranch()
         {
             var bus = new Mock<IBUS>();
