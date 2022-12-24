57bf39240ddf02e8a977ea55c7795c8df5e4d61c
Attila Szücs <ati.szucs@hotmail.com>
2022-12-24 13:10:20 +0100

    LDY imm (0xA0)

--- NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
+++ NESCoreTests/Unit/CPUTest/InstructionsetTests.cs
@@ -116,6 +116,52 @@ namespace NESCoreTests.Unit.CPUTest
         }
 
         [Fact]
+        public void LDY_IMM()
+        {
+            var registers = new Mock<IRegisters>();
+            var bus = new Mock<IBUS>();
+            bus.Setup(b => b.Read(It.IsAny<UInt16>())).Returns(0x10);
+
+            var ldy_imm = new CPUInstructions().InstructionSet[Opcodes.LDY_IMM];
+            var cycles = ldy_imm(bus.Object, registers.Object);
+
+            registers.VerifySet(r => r.Y = 0x10);
+            registers.Verify(r => r.SetNegativeFlag(false));
+            registers.Verify(r => r.SetZeroFlag(false));
+            cycles.Should().Be(2);
+        }
+
+        [Fact]
+        public void LDY_IMM_sets_zero_flag()
+        {
+            var registers = new Mock<IRegisters>();
+            var bus = new Mock<IBUS>();
+            bus.Setup(b => b.Read(It.IsAny<UInt16>())).Returns(0);
+
+            var ldy_imm = new CPUInstructions().InstructionSet[Opcodes.LDY_IMM];
+            var cycles = ldy_imm(bus.Object, registers.Object);
+
+            registers.VerifySet(r => r.Y = 0);
+            registers.Verify(r => r.SetZeroFlag(true));
+            cycles.Should().Be(2);
+        }
+
+        [Fact]
+        public void LDY_IMM_sets_negative_flag()
+        {
+            var registers = new Mock<IRegisters>();
+            var bus = new Mock<IBUS>();
+            bus.Setup(b => b.Read(It.IsAny<UInt16>())).Returns(unchecked((byte)-1));
+
+            var ldy_imm = new CPUInstructions().InstructionSet[Opcodes.LDY_IMM];
+            var cycles = ldy_imm(bus.Object, registers.Object);
+
+            registers.VerifySet(r => r.Y = unchecked((byte)-1));
+            registers.Verify(r => r.SetNegativeFlag(true));
+            cycles.Should().Be(2);
+        }
+
+        [Fact]
         public void STA_ZERO()
         {
             var registers = new Mock<IRegisters>();
