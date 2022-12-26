1ea63748369347eaebade5d50f145411a92a9167
Attila Szücs <ati.szucs@hotmail.com>
2022-12-26 18:22:09 +0100

    Added test ROMs

--- NESCoreTests/Unit/CartridgeParserTests.cs
+++ NESCoreTests/Unit/CartridgeParserTests.cs
@@ -37,14 +37,14 @@ namespace NESCoreTests.Unit
             }
         }
 
-        private const string NESTEST_ROM_PATH = "ROMS/nestest.nes";
-        private const string MARIO_BROS_ROM_PATH = "ROMS/mario.nes";
-        private const string SUPER_MARIO_ROM_PATH = "ROMS/smario.nes";
-        private const string DONKEY_KONG_ROM_PATH = "ROMS/dk.nes";
-        private const string DRAGON_WARRIOR_ROM_PATH = "ROMS/dragon_warrior.nes";
-        private const string RAD_RACER_II_ROM_PATH = "ROMS/rad_racer_2.nes";
-        private const string DEATHBOTS_ROM_PATH = "ROMS/deathbots.nes";
-        private const string DUCKTALESROM_PATH = "ROMS/ducktales.nes";
+        private const string NESTEST_ROM_PATH = "TestROMs/nestest.nes";
+        private const string MARIO_BROS_ROM_PATH = "TestROMs/mario.nes";
+        private const string SUPER_MARIO_ROM_PATH = "TestROMs/smario.nes";
+        private const string DONKEY_KONG_ROM_PATH = "TestROMs/dk.nes";
+        private const string DRAGON_WARRIOR_ROM_PATH = "TestROMs/dragon_warrior.nes";
+        private const string RAD_RACER_II_ROM_PATH = "TestROMs/rad_racer_2.nes";
+        private const string DEATHBOTS_ROM_PATH = "TestROMs/deathbots.nes";
+        private const string DUCKTALESROM_PATH = "TestROMs/ducktales.nes";
 
         [Fact]
         public void HeaderBeginsWithMagicString()
