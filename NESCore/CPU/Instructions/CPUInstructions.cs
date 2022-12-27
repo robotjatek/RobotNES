107961ab64d549caf16b854c946f0156f9b910a9
Attila Szücs <ati.szucs@hotmail.com>
2022-12-27 17:51:04 +0100

    Moved increment instructions into a separate file

--- NESCore/CPU/Instructions/CPUInstructions.cs
+++ NESCore/CPU/Instructions/CPUInstructions.cs
@@ -296,38 +296,6 @@
             return 2;
         }
 
-        private static byte INX(IBUS bus, IRegisters registers)
-        {
-            registers.X++;
-            registers.SetZeroFlag(registers.X == 0);
-            registers.SetNegativeFlag((registers.X & 0x80) > 0);
-            return 2;
-        }
-
-        private static byte INY(IBUS bus, IRegisters registers)
-        {
-            registers.Y++;
-            registers.SetZeroFlag(registers.Y == 0);
-            registers.SetNegativeFlag((registers.Y & 0x80) > 0);
-            return 2;
-        }
-
-        private static byte DEX(IBUS bus, IRegisters registers)
-        {
-            registers.X--;
-            registers.SetZeroFlag(registers.X == 0);
-            registers.SetNegativeFlag((registers.X & 0x80) > 0);
-            return 2;
-        }
-
-        private static byte DEY(IBUS bus, IRegisters registers)
-        {
-            registers.Y--;
-            registers.SetZeroFlag(registers.Y == 0);
-            registers.SetNegativeFlag((registers.Y & 0x80) > 0);
-            return 2;
-        }
-
         private static byte TAX(IBUS bus, IRegisters registers)
         {
             registers.X = registers.A;
---
+++ NESCore/CPU/Instructions/IncrementInstructions.cs
@@ -0,0 +1,37 @@
+﻿namespace NESCore.CPU.Instructions
+{
+    public partial class CPUInstructions
+    {
+        private static byte INX(IBUS bus, IRegisters registers)
+        {
+            registers.X++;
+            registers.SetZeroFlag(registers.X == 0);
+            registers.SetNegativeFlag((registers.X & 0x80) > 0);
+            return 2;
+        }
+
+        private static byte INY(IBUS bus, IRegisters registers)
+        {
+            registers.Y++;
+            registers.SetZeroFlag(registers.Y == 0);
+            registers.SetNegativeFlag((registers.Y & 0x80) > 0);
+            return 2;
+        }
+
+        private static byte DEX(IBUS bus, IRegisters registers)
+        {
+            registers.X--;
+            registers.SetZeroFlag(registers.X == 0);
+            registers.SetNegativeFlag((registers.X & 0x80) > 0);
+            return 2;
+        }
+
+        private static byte DEY(IBUS bus, IRegisters registers)
+        {
+            registers.Y--;
+            registers.SetZeroFlag(registers.Y == 0);
+            registers.SetNegativeFlag((registers.Y & 0x80) > 0);
+            return 2;
+        }
+    }
+}
