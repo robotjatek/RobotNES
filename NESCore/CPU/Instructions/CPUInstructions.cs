924df920b563336103e283d095f958d92afe48d1
Attila Szücs <ati.szucs@hotmail.com>
2022-12-27 18:03:19 +0100

    Moved transfer instructions into a separate file

--- NESCore/CPU/Instructions/CPUInstructions.cs
+++ NESCore/CPU/Instructions/CPUInstructions.cs
@@ -143,53 +143,7 @@
             var address = AddressingAbsoulteAddressOnly(bus, registers).Address;
             bus.Write(address, registers.X);
             return 4; //1(opcode fetch) + 2 (2 byte fetch from memory) + 1 (1 byte write to memory)
-        }
-
-        private static byte TAX(IBUS bus, IRegisters registers)
-        {
-            registers.X = registers.A;
-            registers.SetZeroFlag(registers.X == 0);
-            registers.SetNegativeFlag((registers.X & 0x80) > 0);
-            return 2;
-        }
-
-        private static byte TSX(IBUS bus, IRegisters registers)
-        {
-            registers.X = registers.SP;
-            registers.SetZeroFlag(registers.X == 0);
-            registers.SetNegativeFlag((registers.X & 0x80) > 0);
-            return 2;
-        }
-
-        private static byte TAY(IBUS bus, IRegisters registers)
-        {
-            registers.Y = registers.A;
-            registers.SetZeroFlag(registers.Y == 0);
-            registers.SetNegativeFlag((registers.Y & 0x80) > 0);
-            return 2;
-        }
-
-        private static byte TXA(IBUS bus, IRegisters registers)
-        {
-            registers.A = registers.X;
-            registers.SetZeroFlag(registers.A == 0);
-            registers.SetNegativeFlag((registers.A & 0x80) > 0);
-            return 2;
-        }
-
-        private static byte TYA(IBUS bus, IRegisters registers)
-        {
-            registers.A = registers.Y;
-            registers.SetZeroFlag(registers.A == 0);
-            registers.SetNegativeFlag((registers.A & 0x80) > 0);
-            return 2;
-        }
-
-        private static byte TXS(IBUS bus, IRegisters registers)
-        {
-            registers.SP = registers.X;
-            return 2;
-        }
+        }       
 
         private static byte NOP(IBUS bus, IRegisters registers)
         {
