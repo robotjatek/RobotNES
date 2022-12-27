75869b7126482cb97c73a30c9932fef43f1d0a3d
Attila Szücs <ati.szucs@hotmail.com>
2022-12-27 17:45:34 +0100

    Moved branch instructions into a separate file

---
+++ NESCore/CPU/Instructions/BranchInstructions.cs
@@ -0,0 +1,87 @@
+﻿namespace NESCore.CPU.Instructions
+{
+    public partial class CPUInstructions
+    {
+        private static byte BCC(IBUS bus, IRegisters registers)
+        {
+            return BranchInstruction(bus, registers, (r) =>
+            {
+                return r.GetCarryFlag() == false;
+            });
+        }
+
+        private static byte BEQ(IBUS bus, IRegisters registers)
+        {
+            return BranchInstruction(bus, registers, (r) =>
+            {
+                return r.GetZeroFlag() == true;
+            });
+        }
+
+        private static byte BNE(IBUS bus, IRegisters registers)
+        {
+            return BranchInstruction(bus, registers, (r) =>
+            {
+                return r.GetZeroFlag() == false;
+            });
+        }
+
+        private static byte BCS(IBUS bus, IRegisters registers)
+        {
+            return BranchInstruction(bus, registers, (r) =>
+            {
+                return r.GetCarryFlag() == true;
+            });
+        }
+
+        private static byte BVC(IBUS bus, IRegisters registers)
+        {
+            return BranchInstruction(bus, registers, (r) =>
+            {
+                return r.GetOverflowFlag() == false;
+            });
+        }
+
+        private static byte BVS(IBUS bus, IRegisters registers)
+        {
+            return BranchInstruction(bus, registers, (r) =>
+            {
+                return r.GetOverflowFlag() == true;
+            });
+        }
+
+        private static byte BPL(IBUS bus, IRegisters registers)
+        {
+            return BranchInstruction(bus, registers, (r) =>
+            {
+                return r.GetNegativeFlag() == false;
+            });
+        }
+
+        private static byte BMI(IBUS bus, IRegisters registers)
+        {
+            return BranchInstruction(bus, registers, (r) =>
+            {
+                return r.GetNegativeFlag() == true;
+            });
+        }
+
+        private static byte BranchInstruction(IBUS bus, IRegisters registers, Func<IRegisters, bool> condition)
+        {
+            byte cycles = 2;
+            sbyte offset = (sbyte)Fetch(bus, registers);
+            if (condition(registers))
+            {
+                var oldAddress = registers.PC;
+                registers.PC = (ushort)(registers.PC + offset);
+                cycles++;
+                if ((oldAddress & 0xff00) != (registers.PC & 0xff00))
+                {
+                    cycles++;
+                }
+            }
+
+            return cycles;
+        }
+    }
+}
--- NESCore/CPU/Instructions/CPUInstructions.cs
+++ NESCore/CPU/Instructions/CPUInstructions.cs
@@ -63,70 +63,6 @@
             InstructionSet[Opcodes.LDA_ABS] = LDA_ABS;
         }
 
-        private static byte BCC(IBUS bus, IRegisters registers)
-        {
-            return BranchInstruction(bus, registers, (r) =>
-            {
-                return r.GetCarryFlag() == false;
-            });
-        }
-
-        private static byte BEQ(IBUS bus, IRegisters registers)
-        {
-            return BranchInstruction(bus, registers, (r) =>
-            {
-                return r.GetZeroFlag() == true;
-            });
-        }
-
-        private static byte BNE(IBUS bus, IRegisters registers)
-        {
-            return BranchInstruction(bus, registers, (r) =>
-            {
-                return r.GetZeroFlag() == false;
-            });
-        }
-
-        private static byte BCS(IBUS bus, IRegisters registers)
-        {
-            return BranchInstruction(bus, registers, (r) =>
-            {
-                return r.GetCarryFlag() == true;
-            });
-        }
-
-        private static byte BVC(IBUS bus, IRegisters registers)
-        {
-            return BranchInstruction(bus, registers, (r) =>
-            {
-                return r.GetOverflowFlag() == false;
-            });
-        }
-
-        private static byte BVS(IBUS bus, IRegisters registers)
-        {
-            return BranchInstruction(bus, registers, (r) =>
-            {
-                return r.GetOverflowFlag() == true;
-            });
-        }
-
-        private static byte BPL(IBUS bus, IRegisters registers)
-        {
-            return BranchInstruction(bus, registers, (r) =>
-            {
-                return r.GetNegativeFlag() == false;
-            });
-        }
-
-        private static byte BMI(IBUS bus, IRegisters registers)
-        {
-            return BranchInstruction(bus, registers, (r) =>
-            {
-                return r.GetNegativeFlag() == true;
-            });
-        }
-
         private static byte JMP_ABS(IBUS bus, IRegisters registers)
         {
             var address = Fetch16(bus, registers);
@@ -509,24 +445,6 @@
             var sp = registers.SP + 1;
             registers.SP++;
             return bus.Read((ushort)(0x100 | sp));
-        }
-
-        private static byte BranchInstruction(IBUS bus, IRegisters registers, Func<IRegisters, bool> condition)
-        {
-            byte cycles = 2;
-            sbyte offset = (sbyte)Fetch(bus, registers);
-            if (condition(registers))
-            {
-                var oldAddress = registers.PC;
-                registers.PC = (ushort)(registers.PC + offset);
-                cycles++;
-                if ((oldAddress & 0xff00) != (registers.PC & 0xff00))
-                {
-                    cycles++;
-                }
-            }
-
-            return cycles;
         }
 
         class AddressingResult
