70545f9a1b6f3e94bf8193a382eb43a9fef91deb
Attila Szücs <ati.szucs@hotmail.com>
2022-12-27 17:56:57 +0100

    Moved logic instructions into a separate file

--- NESCore/CPU/Instructions/CPUInstructions.cs
+++ NESCore/CPU/Instructions/CPUInstructions.cs
@@ -145,51 +145,7 @@
             return 4; //1(opcode fetch) + 2 (2 byte fetch from memory) + 1 (1 byte write to memory)
         }
 
-        private static byte BIT_ZERO(IBUS bus, IRegisters registers)
-        {
-            var value = AddressingZeroWithValue(bus, registers).Value;
-
-            var isNegative = (value & 0x80) == 0x80;
-            registers.SetNegativeFlag(isNegative);
-
-            var overflow = (value & 0x40) == 0x40;
-            registers.SetOverflowFlag(overflow);
-
-            var isZero = (value & registers.A) == 0;
-            registers.SetZeroFlag(isZero);
-
-            return 3;
-        }
-
-        private static byte AND_IMM(IBUS bus, IRegisters registers)
-        {
-            var mask = Fetch(bus, registers);
-            var value = (byte)(registers.A & mask);
-            registers.A = value;
-            registers.SetZeroFlag(value == 0);
-            registers.SetNegativeFlag((sbyte)value < 0);
-            return 2;
-        }
-
-        private static byte ORA_IMM(IBUS bus, IRegisters registers)
-        {
-            var mask = Fetch(bus, registers);
-            var value = (byte)(registers.A | mask);
-            registers.A = value;
-            registers.SetZeroFlag(value == 0);
-            registers.SetNegativeFlag((sbyte)value < 0);
-            return 2;
-        }
-
-        private static byte EOR_IMM(IBUS bus, IRegisters registers)
-        {
-            var mask = Fetch(bus, registers);
-            var value = (byte)(registers.A ^ mask);
-            registers.A = value;
-            registers.SetZeroFlag(value == 0);
-            registers.SetNegativeFlag((sbyte)value < 0);
-            return 2;
-        }
+       
 
         private static byte PHA(IBUS bus, IRegisters registers)
         {
---
+++ NESCore/CPU/Instructions/LogicInstructions.cs
@@ -0,0 +1,51 @@
+﻿namespace NESCore.CPU.Instructions
+{
+    public partial class CPUInstructions
+    {
+        private static byte BIT_ZERO(IBUS bus, IRegisters registers)
+        {
+            var value = AddressingZeroWithValue(bus, registers).Value;
+
+            var isNegative = (value & 0x80) == 0x80;
+            registers.SetNegativeFlag(isNegative);
+
+            var overflow = (value & 0x40) == 0x40;
+            registers.SetOverflowFlag(overflow);
+
+            var isZero = (value & registers.A) == 0;
+            registers.SetZeroFlag(isZero);
+
+            return 3;
+        }
+
+        private static byte AND_IMM(IBUS bus, IRegisters registers)
+        {
+            var mask = Fetch(bus, registers);
+            var value = (byte)(registers.A & mask);
+            registers.A = value;
+            registers.SetZeroFlag(value == 0);
+            registers.SetNegativeFlag((sbyte)value < 0);
+            return 2;
+        }
+
+        private static byte ORA_IMM(IBUS bus, IRegisters registers)
+        {
+            var mask = Fetch(bus, registers);
+            var value = (byte)(registers.A | mask);
+            registers.A = value;
+            registers.SetZeroFlag(value == 0);
+            registers.SetNegativeFlag((sbyte)value < 0);
+            return 2;
+        }
+
+        private static byte EOR_IMM(IBUS bus, IRegisters registers)
+        {
+            var mask = Fetch(bus, registers);
+            var value = (byte)(registers.A ^ mask);
+            registers.A = value;
+            registers.SetZeroFlag(value == 0);
+            registers.SetNegativeFlag((sbyte)value < 0);
+            return 2;
+        }
+    }
+}
