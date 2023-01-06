f64a328e05117c0f7cb0dac614ea6477da16143a
Attila Szücs <ati.szucs@hotmail.com>
2023-01-06 19:39:19 +0100

    Moved NOP instructions into separate class

--- NESCore/CPU/Instructions/CPUInstructions.cs
+++ NESCore/CPU/Instructions/CPUInstructions.cs
@@ -188,41 +188,6 @@
             InstructionSet[Opcodes.NOP_ABS_X_FC] = NOP_ABS_X;
         }
 
-        private static byte NOP(IBUS bus, IRegisters registers)
-        {
-            return 2;
-        }
-
-        private static byte NOP_IMM(IBUS bus, IRegisters registers)
-        {
-            var addressingResult = AddressingImmediate(bus, registers);
-            return addressingResult.Cycles;
-        }
-
-        private static byte NOP_ZERO(IBUS bus, IRegisters registers)
-        {
-            var addressingResult = AddressingZero(bus, registers);
-            return addressingResult.Cycles;
-        }
-
-        private static byte NOP_ZERO_X(IBUS bus, IRegisters registers)
-        {
-            var addressingResult = AddressingZeroX(bus, registers);
-            return addressingResult.Cycles;
-        }
-
-        private static byte NOP_ABS(IBUS bus, IRegisters registers)
-        {
-            var addressingResult = AddressingAbsolute(bus, registers);
-            return addressingResult.Cycles;
-        }
-
-        private static byte NOP_ABS_X(IBUS bus, IRegisters registers)
-        {
-            var addressingResult = AddressingAbsoluteX(bus, registers);
-            return addressingResult.Cycles;
-        }
-
         private static ushort Fetch16(IBUS bus, IRegisters registers)
         {
             var low = Fetch(bus, registers);
---
+++ NESCore/CPU/Instructions/NOPInstructions.cs
@@ -0,0 +1,40 @@
+﻿namespace NESCore.CPU.Instructions
+{
+    public partial class CPUInstructions
+    {
+        private static byte NOP(IBUS bus, IRegisters registers)
+        {
+            return 2;
+        }
+
+        private static byte NOP_IMM(IBUS bus, IRegisters registers)
+        {
+            var addressingResult = AddressingImmediate(bus, registers);
+            return addressingResult.Cycles;
+        }
+
+        private static byte NOP_ZERO(IBUS bus, IRegisters registers)
+        {
+            var addressingResult = AddressingZero(bus, registers);
+            return addressingResult.Cycles;
+        }
+
+        private static byte NOP_ZERO_X(IBUS bus, IRegisters registers)
+        {
+            var addressingResult = AddressingZeroX(bus, registers);
+            return addressingResult.Cycles;
+        }
+
+        private static byte NOP_ABS(IBUS bus, IRegisters registers)
+        {
+            var addressingResult = AddressingAbsolute(bus, registers);
+            return addressingResult.Cycles;
+        }
+
+        private static byte NOP_ABS_X(IBUS bus, IRegisters registers)
+        {
+            var addressingResult = AddressingAbsoluteX(bus, registers);
+            return addressingResult.Cycles;
+        }
+    }
+}
