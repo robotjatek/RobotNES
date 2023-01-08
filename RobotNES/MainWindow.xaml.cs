5d7def4d2ab4a06b196392dab8ab9f6d70d4610c
Attila Szücs <ati.szucs@hotmail.com>
2023-01-08 14:05:31 +0100

    Launch ROMs from ROMs folder using the UI

--- RobotNES/MainWindow.xaml.cs
+++ RobotNES/MainWindow.xaml.cs
@@ -22,43 +22,27 @@ namespace RobotNES
 {
     public partial class MainWindow : Window
     {
-        private readonly NesSystem _nesSystem;
+        private NesSystem? _nesSystem;
         private readonly ILogger _logger;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            if(File.Exists("RobotNES.log"))
-            {
-                File.Delete("RobotNES.log");
-            }
-
             _logger = CreateLogger();
-
-            _nesSystem = new NesSystem("nestest.nes", _logger);
-        }
-
-        private async void Start_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                await Task.Run(() => _nesSystem.Run());
-            }
-            catch(Exception ex)
-            {
-                // log any uncatched errors
-                _logger.Fatal(ex, "Unhandled error.");
-            }
         }
 
         private async void Stop_Click(object sender, RoutedEventArgs e)
         {
-            await Task.Run(() => _nesSystem.Stop());
+            await Task.Run(() => _nesSystem?.Stop());
         }
 
         private static ILogger CreateLogger()
         {
+            if (File.Exists("RobotNES.log"))
+            {
+                File.Delete("RobotNES.log");
+            }
+
             var logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .WriteTo
@@ -67,6 +51,20 @@ namespace RobotNES
                 .CreateLogger();
 
             return logger;
+        }
+        private async void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                var file = (NesFile)(((ListViewItem)sender).Content);
+                _nesSystem = new NesSystem(file.Path, _logger);
+                await Task.Run(() => _nesSystem?.Run());
+            }
+            catch (Exception ex)
+            {
+                // log any uncatched errors
+                _logger.Fatal(ex, "Unhandled error.");
+            }
         }
     }
 }
