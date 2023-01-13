77f3f8012d355c36d1726c41a2883a309a8629c3
Attila Szücs <ati.szucs@hotmail.com>
2023-01-13 18:42:57 +0100

    Unknown instuction test

---
+++ NESCoreTests/Unit/CPUTest/Instructions/UnknownInstructionTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+
+using NESCore;
+using NESCore.CPU;
+
+namespace NESCoreTests.Unit.CPUTest.Instructions
+{
+    public class UnknownInstructionTests : InstructionTestBase
+    {
+        public UnknownInstructionTests(InstructionsFixture fixture) : base(fixture)
+        {
+        }
+
+        [Fact]
+        public void CpuThrowsOnUnknownInstruction()
+        {
+            var unknownOpcode = 0x02;
+            var registers = Mock.Of<IRegisters>();
+            var bus = Mock.Of<IBUS>();
+            var instruction = _instructions[unknownOpcode];
+            var sut = () => instruction(bus, registers);
+            sut.Should().Throw<NotImplementedException>();
+        }
+    }
+}
